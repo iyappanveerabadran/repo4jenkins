321b9964963ed059037164bb0ca4cf57ed55d318
iyappanveerabadran <51178126+iyappanveerabadran@users.noreply.github.com>
2019-08-01 08:57:41 +0530

    Update 2HelloWorld.java

--- 2HelloWorld.java
+++ 2HelloWorld.java
@@ -1,8 +1,8 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class 2HelloWorld {
+public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("2Hello, World");
+    System.out.println("Hello, World2");
   }
 }
