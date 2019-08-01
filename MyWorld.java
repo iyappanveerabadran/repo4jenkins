c3419ce245710bac3f2af0a0571b0544a0c45645
iyappanveerabadran <51178126+iyappanveerabadran@users.noreply.github.com>
2019-08-01 08:58:58 +0530

    MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,8 +1,8 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class MyWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World2");
+    System.out.println("My, World2");
   }
 }
